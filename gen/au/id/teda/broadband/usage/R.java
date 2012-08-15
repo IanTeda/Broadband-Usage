fb3ae52f441767c67645e5606f455477d31a30ee
Ian Teda <ian@teda.id.au>
2012-08-15 17:39:56 +1000

    Added settings and refresh to action bar

--- gen/au/id/teda/broadband/usage/R.java
+++ gen/au/id/teda/broadband/usage/R.java
@@ -11,10 +11,13 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int ic_action_search=0x7f020000;
-        public static final int ic_launcher=0x7f020001;
+        public static final int ic_action_refresh=0x7f020000;
+        public static final int ic_action_search=0x7f020001;
+        public static final int ic_action_settings=0x7f020002;
+        public static final int ic_launcher=0x7f020003;
     }
     public static final class id {
+        public static final int menu_refresh=0x7f070002;
         public static final int menu_settings=0x7f070001;
         public static final int pager=0x7f070000;
     }
@@ -22,13 +25,16 @@ public final class R {
         public static final int activity_main=0x7f030000;
     }
     public static final class menu {
-        public static final int activity_main=0x7f060000;
+        public static final int options_menu=0x7f060000;
     }
     public static final class string {
         public static final int app_name=0x7f040000;
         public static final int hello_world=0x7f040004;
-        public static final int menu_settings=0x7f040005;
-        public static final int title_activity_main=0x7f040006;
+        public static final int menu_refresh=0x7f040007;
+        /**  Options Menu 
+         */
+        public static final int menu_settings=0x7f040006;
+        public static final int title_activity_main=0x7f040005;
         public static final int title_section1=0x7f040003;
         public static final int title_section2=0x7f040002;
         public static final int title_section3=0x7f040001;
