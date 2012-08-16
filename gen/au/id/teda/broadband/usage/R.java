9584b5d91dcaf7bb2a5e2b1641103bced2622947
Ian Teda <ian@teda.id.au>
2012-08-17 09:47:38 +1000

    Added action bar tabs

--- gen/au/id/teda/broadband/usage/R.java
+++ gen/au/id/teda/broadband/usage/R.java
@@ -8,9 +8,6 @@
 package au.id.teda.broadband.usage;
 
 public final class R {
-    public static final class array {
-        public static final int action_bar_spinner_list=0x7f040000;
-    }
     public static final class attr {
     }
     public static final class drawable {
@@ -20,29 +17,36 @@ public final class R {
         public static final int ic_launcher=0x7f020003;
     }
     public static final class id {
-        public static final int menu_refresh=0x7f080002;
-        public static final int menu_settings=0x7f080001;
-        public static final int pager=0x7f080000;
+        public static final int menu_refresh=0x7f070002;
+        public static final int menu_settings=0x7f070001;
+        public static final int pager=0x7f070000;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
     }
     public static final class menu {
-        public static final int options_menu=0x7f070000;
+        public static final int options_menu=0x7f060000;
     }
     public static final class string {
-        public static final int app_name=0x7f050000;
-        public static final int hello_world=0x7f050004;
-        public static final int menu_refresh=0x7f050007;
+        public static final int action_bar_tab_analysis=0x7f040002;
+        /**  Action bar tabs 
+         */
+        public static final int action_bar_tab_current=0x7f040001;
+        public static final int action_bar_tab_data_table=0x7f040003;
+        public static final int app_name=0x7f040006;
+        public static final int hello_world=0x7f04000a;
+        public static final int menu_refresh=0x7f040005;
         /**  Options Menu 
          */
-        public static final int menu_settings=0x7f050006;
-        public static final int title_activity_main=0x7f050005;
-        public static final int title_section1=0x7f050003;
-        public static final int title_section2=0x7f050002;
-        public static final int title_section3=0x7f050001;
+        public static final int menu_settings=0x7f040004;
+        /**  Title on action bar 
+         */
+        public static final int title_activity_main=0x7f040000;
+        public static final int title_section1=0x7f040009;
+        public static final int title_section2=0x7f040008;
+        public static final int title_section3=0x7f040007;
     }
     public static final class style {
-        public static final int AppTheme=0x7f060000;
+        public static final int AppTheme=0x7f050000;
     }
 }
--- src/au/id/teda/broadband/usage/MainActivity.java
+++ src/au/id/teda/broadband/usage/MainActivity.java
@@ -2,49 +2,64 @@ package au.id.teda.broadband.usage;
 
 import android.app.ActionBar;
 import android.app.Activity;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
-import android.widget.ArrayAdapter;
-import android.widget.SpinnerAdapter;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements ActionBar.TabListener {
 	
 
 	// Static string for debug tags
-	private static final String DEBUG_TAG = MainActivity.class.getSimpleName();
+	private static final String DEBUG_TAG = "Broadband Usage";
+	//private static final String INFO_TAG = MainActivity.class.getSimpleName();
 	
     @Override
     public void onCreate(Bundle savedInstanceState) {
     	   super.onCreate(savedInstanceState);
     	   setContentView(R.layout.activity_main);
     	   
-    	   // Get action bar instance
-    	   ActionBar actionBar = getActionBar();
-    	   
-    	   // Set action bar icon for navigation
-    	   actionBar.setDisplayHomeAsUpEnabled(true);
-    	   
-    	   // Set action bar spinner adapter
-    	   SpinnerAdapter mSpinnerAdapter = ArrayAdapter.createFromResource(this,
-    			   R.array.action_bar_spinner_list,
-    			   android.R.layout.simple_spinner_dropdown_item);
-    	   
-    	   mOnNavigationListener = new OnNavigationListener() {
-    		   	// Get the same strings provided for the drop-down's ArrayAdapter
-    		   String[] strings = getResources().getStringArray(R.array.action_list);
-
-    		   @Override
-    		   public boolean onNavigationItemSelected(int position, long itemId) {
-	    		  
-    		   }
-	    	};
-    	   
-    	   // Allow action bar spinner navigation
-    	   actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_LIST);
-    	   // Set call backs for action bar spinner
-    	   actionBar.setListNavigationCallbacks(mSpinnerAdapter, mNavigationCallback);
+    	   setUpActionBar();
     	   
     }
+
+	/**
+	 * Method used to set up the action bar
+	 * 1. Enable icon home up navigation
+	 * 2. Enable tab navigation
+	 * 3. Set up tabs
+	 */
+	private void setUpActionBar() {
+		// Get action bar instance
+		ActionBar actionBar = getActionBar();
+    	   
+		// Set action bar icon for navigation
+		actionBar.setDisplayHomeAsUpEnabled(true);
+    	   
+		// Set up action bar tabs
+		actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+		// First tab
+		actionBar.addTab(
+				actionBar
+					.newTab()
+					.setText(R.string.action_bar_tab_current)
+					.setTabListener(this) // Set MainActivity as tab listener
+		);
+		// Second tab
+		actionBar.addTab(
+				actionBar
+					.newTab()
+					.setText(R.string.action_bar_tab_analysis)
+					.setTabListener(this) // Set MainActivity as tab listener
+		);
+		// Third tab
+		actionBar.addTab(
+				actionBar
+					.newTab()
+					.setText(R.string.action_bar_tab_data_table)
+					.setTabListener(this) // Set MainActivity as tab listener
+		);
+	}
        
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -53,4 +68,23 @@ public class MainActivity extends Activity {
         getMenuInflater().inflate(R.menu.options_menu, menu);
         return true;
     }
+    
+    @Override
+    public void onTabUnselected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {	
+    	Log.d(DEBUG_TAG, "onTabUnselected");
+    }
+	
+    @Override
+	public void onTabSelected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {
+    	// When the given tab is selected, switch to the corresponding page in the ViewPager.
+    	Log.d(DEBUG_TAG, "onTabSelected");
+    }
+	
+    @Override
+	public void onTabReselected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {
+    	Log.d(DEBUG_TAG, "onTabReselected");
+    
+    }
+    
+ 
 }
