8a0a5a17d8d8fcdf875af95d8b6d102cc2b13064
Ian Teda <ian@teda.id.au>
2012-08-16 16:52:14 +1000

    Trying to add a spinner

--- gen/au/id/teda/broadband/usage/R.java
+++ gen/au/id/teda/broadband/usage/R.java
@@ -8,6 +8,9 @@
 package au.id.teda.broadband.usage;
 
 public final class R {
+    public static final class array {
+        public static final int action_bar_spinner_list=0x7f040000;
+    }
     public static final class attr {
     }
     public static final class drawable {
@@ -17,29 +20,29 @@ public final class R {
         public static final int ic_launcher=0x7f020003;
     }
     public static final class id {
-        public static final int menu_refresh=0x7f070002;
-        public static final int menu_settings=0x7f070001;
-        public static final int pager=0x7f070000;
+        public static final int menu_refresh=0x7f080002;
+        public static final int menu_settings=0x7f080001;
+        public static final int pager=0x7f080000;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
     }
     public static final class menu {
-        public static final int options_menu=0x7f060000;
+        public static final int options_menu=0x7f070000;
     }
     public static final class string {
-        public static final int app_name=0x7f040000;
-        public static final int hello_world=0x7f040004;
-        public static final int menu_refresh=0x7f040007;
+        public static final int app_name=0x7f050000;
+        public static final int hello_world=0x7f050004;
+        public static final int menu_refresh=0x7f050007;
         /**  Options Menu 
          */
-        public static final int menu_settings=0x7f040006;
-        public static final int title_activity_main=0x7f040005;
-        public static final int title_section1=0x7f040003;
-        public static final int title_section2=0x7f040002;
-        public static final int title_section3=0x7f040001;
+        public static final int menu_settings=0x7f050006;
+        public static final int title_activity_main=0x7f050005;
+        public static final int title_section1=0x7f050003;
+        public static final int title_section2=0x7f050002;
+        public static final int title_section3=0x7f050001;
     }
     public static final class style {
-        public static final int AppTheme=0x7f050000;
+        public static final int AppTheme=0x7f060000;
     }
 }
--- src/au/id/teda/broadband/usage/MainActivity.java
+++ src/au/id/teda/broadband/usage/MainActivity.java
@@ -4,6 +4,8 @@ import android.app.ActionBar;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.widget.ArrayAdapter;
+import android.widget.SpinnerAdapter;
 
 public class MainActivity extends Activity {
 	
@@ -16,9 +18,32 @@ public class MainActivity extends Activity {
     	   super.onCreate(savedInstanceState);
     	   setContentView(R.layout.activity_main);
     	   
-    	   // Set action bar icon for navigation
+    	   // Get action bar instance
     	   ActionBar actionBar = getActionBar();
+    	   
+    	   // Set action bar icon for navigation
     	   actionBar.setDisplayHomeAsUpEnabled(true);
+    	   
+    	   // Set action bar spinner adapter
+    	   SpinnerAdapter mSpinnerAdapter = ArrayAdapter.createFromResource(this,
+    			   R.array.action_bar_spinner_list,
+    			   android.R.layout.simple_spinner_dropdown_item);
+    	   
+    	   mOnNavigationListener = new OnNavigationListener() {
+    		   	// Get the same strings provided for the drop-down's ArrayAdapter
+    		   String[] strings = getResources().getStringArray(R.array.action_list);
+
+    		   @Override
+    		   public boolean onNavigationItemSelected(int position, long itemId) {
+	    		  
+    		   }
+	    	};
+    	   
+    	   // Allow action bar spinner navigation
+    	   actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_LIST);
+    	   // Set call backs for action bar spinner
+    	   actionBar.setListNavigationCallbacks(mSpinnerAdapter, mNavigationCallback);
+    	   
     }
        
     @Override
