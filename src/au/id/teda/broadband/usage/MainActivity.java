4edd0a78a848cb03d72d164928d5596b17aa2a50
Ian Teda <ian@teda.id.au>
2012-08-23 13:08:00 +1000

    Reloaded base template

--- src/au/id/teda/broadband/usage/MainActivity.java
+++ src/au/id/teda/broadband/usage/MainActivity.java
@@ -1,113 +1,147 @@
 package au.id.teda.broadband.usage;
 
 import android.app.ActionBar;
-import android.app.Activity;
 import android.app.FragmentTransaction;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
-import android.util.Log;
+import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.Menu;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
-public class MainActivity extends Activity implements ActionBar.TabListener {
-	
+public class MainActivity extends FragmentActivity implements ActionBar.TabListener {
 
-	// Static string for debug tags
-	private static final String DEBUG_TAG = "Broadband Usage";
-	//private static final String INFO_TAG = MainActivity.class.getSimpleName();
-	
-	/**
+    /**
      * The {@link android.support.v4.view.PagerAdapter} that will provide fragments for each of the
      * sections. We use a {@link android.support.v4.app.FragmentPagerAdapter} derivative, which will
      * keep every loaded fragment in memory. If this becomes too memory intensive, it may be best
      * to switch to a {@link android.support.v4.app.FragmentStatePagerAdapter}.
      */
-	SectionsPagerAdapter mSectionsPagerAdapter;
-	
-	/**
+    SectionsPagerAdapter mSectionsPagerAdapter;
+
+    /**
      * The {@link ViewPager} that will host the section contents.
      */
     ViewPager mViewPager;
-	
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
-    	   super.onCreate(savedInstanceState);
-    	   setContentView(R.layout.activity_main);
-    	   
-    	   // Create the adapter that will return a fragment for each of the three primary sections
-           // of the app.
-           mSectionsPagerAdapter = new SectionsPagerAdapter(getFragmentManager());
-    	   
-           // Setup action bar
-    	   setUpActionBar();
-    	   
-    	   // Set up the ViewPager with the sections adapter.
-           mViewPager = (ViewPager) findViewById(R.id.pager);
-           mViewPager.setAdapter(mSectionsPagerAdapter);
-    	   
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        // Create the adapter that will return a fragment for each of the three primary sections
+        // of the app.
+        mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
+
+        // Set up the action bar.
+        final ActionBar actionBar = getActionBar();
+        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+
+        // Set up the ViewPager with the sections adapter.
+        mViewPager = (ViewPager) findViewById(R.id.pager);
+        mViewPager.setAdapter(mSectionsPagerAdapter);
+
+        // When swiping between different sections, select the corresponding tab.
+        // We can also use ActionBar.Tab#select() to do this if we have a reference to the
+        // Tab.
+        mViewPager.setOnPageChangeListener(new ViewPager.SimpleOnPageChangeListener() {
+            @Override
+            public void onPageSelected(int position) {
+                actionBar.setSelectedNavigationItem(position);
+            }
+        });
+
+        // For each of the sections in the app, add a tab to the action bar.
+        for (int i = 0; i < mSectionsPagerAdapter.getCount(); i++) {
+            // Create a tab with text corresponding to the page title defined by the adapter.
+            // Also specify this Activity object, which implements the TabListener interface, as the
+            // listener for when this tab is selected.
+            actionBar.addTab(
+                    actionBar.newTab()
+                            .setText(mSectionsPagerAdapter.getPageTitle(i))
+                            .setTabListener(this));
+        }
     }
 
-	/**
-	 * Method used to set up the action bar
-	 * 1. Enable icon home up navigation
-	 * 2. Enable tab navigation
-	 * 3. Set up tabs
-	 */
-	private void setUpActionBar() {
-		// Get action bar instance
-		ActionBar actionBar = getActionBar();
-    	   
-		// Set action bar icon for navigation
-		actionBar.setDisplayHomeAsUpEnabled(true);
-    	   
-		// Set up action bar tabs
-		actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
-		// First tab
-		actionBar.addTab(
-				actionBar
-					.newTab()
-					.setText(R.string.action_bar_tab_current)
-					.setTabListener(this) // Set MainActivity as tab listener
-		);
-		// Second tab
-		actionBar.addTab(
-				actionBar
-					.newTab()
-					.setText(R.string.action_bar_tab_analysis)
-					.setTabListener(this) // Set MainActivity as tab listener
-		);
-		// Third tab
-		actionBar.addTab(
-				actionBar
-					.newTab()
-					.setText(R.string.action_bar_tab_data_table)
-					.setTabListener(this) // Set MainActivity as tab listener
-		);
-	}
-       
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-    	// Action bar is included in all activities that use the Theme.Holo theme
-    	// (or one of its descendants)
         getMenuInflater().inflate(R.menu.options_menu, menu);
         return true;
     }
+
     
+
     @Override
-    public void onTabUnselected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {	
-    	Log.d(DEBUG_TAG, "onTabUnselected");
+    public void onTabUnselected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {
     }
-	
+
     @Override
-	public void onTabSelected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {
-    	// When the given tab is selected, switch to the corresponding page in the ViewPager.
-    	Log.d(DEBUG_TAG, "onTabSelected");
+    public void onTabSelected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {
+        // When the given tab is selected, switch to the corresponding page in the ViewPager.
+        mViewPager.setCurrentItem(tab.getPosition());
     }
-	
+
     @Override
-	public void onTabReselected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {
-    	Log.d(DEBUG_TAG, "onTabReselected");
-    
+    public void onTabReselected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {
     }
-    
- 
+
+    /**
+     * A {@link FragmentPagerAdapter} that returns a fragment corresponding to one of the primary
+     * sections of the app.
+     */
+    public class SectionsPagerAdapter extends FragmentPagerAdapter {
+
+        public SectionsPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int i) {
+            Fragment fragment = new DummySectionFragment();
+            Bundle args = new Bundle();
+            args.putInt(DummySectionFragment.ARG_SECTION_NUMBER, i + 1);
+            fragment.setArguments(args);
+            return fragment;
+        }
+
+        @Override
+        public int getCount() {
+            return 3;
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            switch (position) {
+                case 0: return getString(R.string.action_bar_tab_current).toUpperCase();
+                case 1: return getString(R.string.action_bar_tab_analysis).toUpperCase();
+                case 2: return getString(R.string.action_bar_tab_data_table).toUpperCase();
+            }
+            return null;
+        }
+    }
+
+    /**
+     * A dummy fragment representing a section of the app, but that simply displays dummy text.
+     */
+    public static class DummySectionFragment extends Fragment {
+        public DummySectionFragment() {
+        }
+
+        public static final String ARG_SECTION_NUMBER = "section_number";
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                Bundle savedInstanceState) {
+            TextView textView = new TextView(getActivity());
+            textView.setGravity(Gravity.CENTER);
+            Bundle args = getArguments();
+            textView.setText(Integer.toString(args.getInt(ARG_SECTION_NUMBER)));
+            return textView;
+        }
+    }
 }
