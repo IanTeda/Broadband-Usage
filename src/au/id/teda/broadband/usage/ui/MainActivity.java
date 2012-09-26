ec21cb29cdf470a0994fbae4a39e62c4dad574fc
Ian Teda <ian@teda.id.au>
2012-09-26 12:48:42 +1000

    Playing with fragments

--- src/au/id/teda/broadband/usage/ui/MainActivity.java
+++ src/au/id/teda/broadband/usage/ui/MainActivity.java
@@ -20,7 +20,7 @@ import android.widget.TextView;
 import au.id.teda.broadband.usage.R;
 import au.id.teda.broadband.usage.adapter.SectionPagerAdapter;
 
-public class MainActivity extends Activity implements ActionBar.TabListener {
+public class MainActivity extends Activity implements ActionBar.TabListener, MonthListFragment.MonthListSelectedListner {
 
     /**
      * The {@link android.support.v4.view.PagerAdapter} that will provide layouts for each of the
@@ -92,5 +92,40 @@ public class MainActivity extends Activity implements ActionBar.TabListener {
     @Override
     public void onTabReselected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {
     }
+
+	@Override
+	public void onMonthSelected(int position) {
+		// The user selected the month from the MonthListFragment
+		
+		// Capture the article fragment from the activity layout
+        AnalysisFragment articleFrag = (AnalysisFragment)
+                getFragmentManager().findFragmentById(R.id.analysis_fragment_right);
+        
+        if (analysisFrag != null) {
+            // If article frag is available, we're in two-pane layout...
+
+            // Call a method in the ArticleFragment to update its content
+        	analysisFrag.updateArticleView(position);
+
+        } else {
+            // If the frag is not available, we're in the one-pane layout and must swap frags...
+
+            // Create fragment and give it an argument for the selected article
+            ArticleFragment newFragment = new ArticleFragment();
+            Bundle args = new Bundle();
+            args.putInt(ArticleFragment.ARG_POSITION, position);
+            newFragment.setArguments(args);
+            FragmentTransaction transaction = getFragmentManager().beginTransaction();
+
+            // Replace whatever is in the fragment_container view with this fragment,
+            // and add the transaction to the back stack so the user can navigate back
+            transaction.replace(R.id.fragment_container, newFragment);
+            transaction.addToBackStack(null);
+
+            // Commit the transaction
+            transaction.commit();
+        }
+        
+	}
 
 }
