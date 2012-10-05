d8ef2bc157d6f30199951c9643fba808f3512b28
Ian Teda <ian@teda.id.au>
2012-10-05 13:37:26 +1000

    Playing with actionbar spinners

--- src/au/id/teda/broadband/usage/adapter/ActionbarSpinnerAdapter.java
+++ src/au/id/teda/broadband/usage/adapter/ActionbarSpinnerAdapter.java
@@ -1,17 +1,10 @@
 package au.id.teda.broadband.usage.adapter;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
 import android.content.Context;
-import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.SpinnerAdapter;
-import android.widget.TextView;
 
 public class ActionbarSpinnerAdapter extends BaseAdapter implements SpinnerAdapter {
 
@@ -30,7 +23,8 @@ public class ActionbarSpinnerAdapter extends BaseAdapter implements SpinnerAdapt
 
 	@Override
 	public Object getItem(int position){
-		return mSpinnerList.get(itemPosition);
+		return position;
+		
 	}
 
 	@Override
@@ -40,22 +34,7 @@ public class ActionbarSpinnerAdapter extends BaseAdapter implements SpinnerAdapt
 
 	@Override
 	public View getView(int position, View convertView, ViewGroup parent) {
-		
-		View mView = convertView;
-		 
-        // Try to reuse views as much as possible.
-        // It is alot faster than inflating new views all the time
-        // and it saves quite a bit on memory usage aswell.
-        if (convertView == null) {
-            // inflate a new layout for the view.
-        	mView = mLayoutInflater.inflate(android.R.layout.simple_spinner_dropdown_item, parent, false);
-        }
- 
-        TextView mTextView = (TextView) mView.findViewById(android.R.id.text1);
-        // FindViewById<TextView>(Resource.Id.DisplayTextLabel);
-        mTextView.setText((CharSequence) mSpinnerList.get(position));
-        
-        return mView;
+		return parent;
 	}
 
 }
