3b9a24cd2d72a4a8d1283928a5b0a453014a763b
Ian Teda <ian@teda.id.au>
2012-11-17 09:07:09 +1100

    Authenticator activity showing up!

--- src/au/id/teda/broadband/usage/authenticator/AuthenticatationService.java
+++ src/au/id/teda/broadband/usage/authenticator/AuthenticatationService.java
@@ -10,28 +10,21 @@ public class AuthenticatationService extends Service {
 	private static final String DEBUG_TAG = "bbusage";
 	
 	private Authenticator mAccountAuthenticator;
-	
+
     @Override
     public void onCreate() {
-        if (Log.isLoggable(DEBUG_TAG, Log.VERBOSE)) {
-            Log.v(DEBUG_TAG, "Authentication Service started.");
-        }
+    	Log.d(DEBUG_TAG, "Authentication Service started.");
         mAccountAuthenticator = new Authenticator(this);
     }
 
     @Override
     public void onDestroy() {
-        if (Log.isLoggable(DEBUG_TAG, Log.VERBOSE)) {
-            Log.v(DEBUG_TAG, "Authentication Service stopped.");
-        }
+    	Log.d(DEBUG_TAG, "Authentication Service stopped.");
     }
     
     @Override
     public IBinder onBind(Intent intent) {
-        if (Log.isLoggable(DEBUG_TAG, Log.VERBOSE)) {
-            Log.v(DEBUG_TAG, "getBinder()...  returning the AccountAuthenticator binder for intent "
-                    + intent);
-        }
+    	Log.d(DEBUG_TAG, "getBinder()...  returning the AccountAuthenticator binder for intent " + intent);
         return mAccountAuthenticator.getIBinder();
     }
 }
--- src/au/id/teda/broadband/usage/authenticator/Authenticator.java
+++ src/au/id/teda/broadband/usage/authenticator/Authenticator.java
@@ -33,24 +33,28 @@ public class Authenticator extends AbstractAccountAuthenticator {
             String authTokenType, String[] requiredFeatures, Bundle options)
 			throws NetworkErrorException {
 		
-        Log.v(DEBUG_TAG, "addAccount()");
-        final Intent intent = new Intent(mContext, AuthenticatorActivity.class);
+		Log.d(DEBUG_TAG, "addAccount()");
+		
+		final Bundle result = new Bundle();
+		final Intent intent = new Intent(mContext, AuthenticatorActivity.class);
+
         intent.putExtra(AccountManager.KEY_ACCOUNT_AUTHENTICATOR_RESPONSE, response);
-        final Bundle bundle = new Bundle();
-        bundle.putParcelable(AccountManager.KEY_INTENT, intent);
-        return bundle;
+        result.putParcelable(AccountManager.KEY_INTENT, intent);
+        
+        Log.d(DEBUG_TAG, "addAccount() return bundle");
+        
+        return result;
 	}
 
     @Override
-    public Bundle confirmCredentials(
-            AccountAuthenticatorResponse response, Account account, Bundle options) {
-        Log.v(DEBUG_TAG, "confirmCredentials()");
+    public Bundle confirmCredentials(AccountAuthenticatorResponse response, Account account, Bundle options) {
+        Log.d(DEBUG_TAG, "confirmCredentials()");
         return null;
     }
 
     @Override
     public Bundle editProperties(AccountAuthenticatorResponse response, String accountType) {
-        Log.v(DEBUG_TAG, "editProperties()");
+        Log.d(DEBUG_TAG, "editProperties()");
         return null;
     }
 
@@ -58,27 +62,27 @@ public class Authenticator extends AbstractAccountAuthenticator {
 	public Bundle getAuthToken(AccountAuthenticatorResponse response,
 			Account account, String authTokenType, Bundle options)
 			throws NetworkErrorException {
-		Log.v(DEBUG_TAG, "getAuthToken()");
+		Log.d(DEBUG_TAG, "getAuthToken()");
 		return null;
 	}
 
 	@Override
 	public String getAuthTokenLabel(String authTokenType) {
-		Log.v(DEBUG_TAG, "getAuthTokenLabel()");
+		Log.d(DEBUG_TAG, "getAuthTokenLabel()");
 		return null;
 	}
 
 	@Override
 	public Bundle hasFeatures(AccountAuthenticatorResponse response,
 			Account account, String[] features) throws NetworkErrorException {
-		Log.v(DEBUG_TAG, "hasFeatures()");
+		Log.d(DEBUG_TAG, "hasFeatures()");
 		return null;
 	}
 
     @Override
     public Bundle updateCredentials(AccountAuthenticatorResponse response, Account account,
             String authTokenType, Bundle loginOptions) {
-        Log.v(DEBUG_TAG, "updateCredentials()");
+        Log.d(DEBUG_TAG, "updateCredentials()");
         return null;
     }
 
