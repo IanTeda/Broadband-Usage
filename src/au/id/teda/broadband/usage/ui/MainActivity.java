8256d4703046ef4973860e9baa6a9a6fa49c27ee
Ian Teda <ian@teda.id.au>
2012-11-27 16:18:58 +1100

    Action bar refresh icon animation

--- src/au/id/teda/broadband/usage/ui/MainActivity.java
+++ src/au/id/teda/broadband/usage/ui/MainActivity.java
@@ -4,12 +4,18 @@ import com.actionbarsherlock.app.ActionBar;
 import com.actionbarsherlock.app.SherlockFragmentActivity;
 import com.actionbarsherlock.view.Menu;
 import com.actionbarsherlock.view.MenuItem;
+import com.actionbarsherlock.view.Window;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.FragmentManager;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import au.id.teda.broadband.usage.R;
@@ -26,6 +32,8 @@ public class MainActivity extends SherlockFragmentActivity
 	
 	private static final String DEBUG_TAG = "bbusage";
 	
+	private MenuItem refreshItem;
+	
 	FragmentManager mFragmentManager;
 	FragmentLogInDialog fragmentLogInDialog;
 	
@@ -35,6 +43,10 @@ public class MainActivity extends SherlockFragmentActivity
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        
+        // Spin refersh icon on click
+        //requestWindowFeature(Window.FEATURE_INDETERMINATE_PROGRESS);
+        
         setContentView(R.layout.activity_main);
 
         //Remove title bar 
@@ -44,6 +56,7 @@ public class MainActivity extends SherlockFragmentActivity
         final ActionBar mActionBar = getSupportActionBar();
         // Set action bar icon for navigation
         mActionBar.setDisplayHomeAsUpEnabled(true);
+        
         
     }
 
@@ -64,6 +77,8 @@ public class MainActivity extends SherlockFragmentActivity
                 return true;
         case R.id.menu_refresh:
         		Log.d(DEBUG_TAG, "onOptionsItemSelected R.id.menu_refresh");
+        		refreshItem = item;
+        		refresh();
                 return true;
         default:
                 return super.onOptionsItemSelected(item);
@@ -71,8 +86,10 @@ public class MainActivity extends SherlockFragmentActivity
     }
     
 	public void onLogInClick(View button) {
-		NetworkUtilities mNetworkUtilities = new NetworkUtilities(this);
-		mNetworkUtilities.getXmlData();
+		completeRefresh();
+		
+		//NetworkUtilities mNetworkUtilities = new NetworkUtilities(this);
+		//mNetworkUtilities.getXmlData();
 		
 		//AccountInfoHelper mAccountInfoHelper = new AccountInfoHelper(this);
 		//Toast.makeText(this, "Authentication: " + mAccountInfoHelper.getPlan(), Toast.LENGTH_SHORT).show();
@@ -88,6 +105,38 @@ public class MainActivity extends SherlockFragmentActivity
 		//startActivity(authenticatorActivityIntent);
 		
     }
+	
+	 public void refresh() {
+	     animateActionBarRefreshIcon();
+
+	     //TODO trigger loading
+	 }
+
+	/**
+	 * Start animation of refresh icon in action bar
+	 */
+	private void animateActionBarRefreshIcon() {
+		// Attach a rotating ImageView to the refresh item as an ActionView
+	     LayoutInflater inflater = (LayoutInflater) this.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+	     ImageView iv = (ImageView) inflater.inflate(R.layout.refresh_action_view, null);
+
+	     // Set animation
+	     Animation rotation = AnimationUtils.loadAnimation(this, R.anim.clockwise_refresh);
+	     rotation.setRepeatCount(Animation.INFINITE);
+	     iv.startAnimation(rotation);
+
+	     // Start animation of image view
+	     refreshItem.setActionView(iv);
+	}
+	 
+	 public void completeRefresh() {
+		 
+		 // Stop refresh icon animation
+		 if (refreshItem != null && refreshItem.getActionView() != null){
+			 refreshItem.getActionView().clearAnimation();
+			 refreshItem.setActionView(null);
+		 }    
+	}
     
 	@Override
 	public void onFinishLogInListner(String username, String password) {
