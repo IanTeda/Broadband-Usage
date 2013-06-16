e3e46aabb98762d071a800007a612bdddbac5a88
Teeds (Macbook Pro) <ian@teda.id.au>
2013-06-16 12:24:32 +1000

    Added viewpager and indicator to main activity

--- src/au/id/teda/broadband/dev/activity/MainActivity.java
+++ src/au/id/teda/broadband/dev/activity/MainActivity.java
@@ -2,13 +2,25 @@ package au.id.teda.broadband.dev.activity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.util.Log;
 import au.id.teda.broadband.dev.R;
 import au.id.teda.broadband.dev.authenticator.AccountAuthenticator;
 import au.id.teda.broadband.dev.authenticator.AuthenticatorActivity;
+import au.id.teda.broadband.dev.fragments.*;
+import com.viewpagerindicator.LinePageIndicator;
+import com.viewpagerindicator.TitlePageIndicator;
+import com.viewpagerindicator.UnderlinePageIndicator;
 
 public class MainActivity extends BaseActivity {
-	
+
+    private MainActivityPagerAdapter mAdapter;
+    private ViewPager mPager;
+    private LinePageIndicator mIndicator;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,7 +45,47 @@ public class MainActivity extends BaseActivity {
             Log.d(DEBUG_TAG, "activity_main");
             setContentView(R.layout.activity_main);
         }
-        
+
+        mAdapter = new MainActivityPagerAdapter(getSupportFragmentManager());
+        mPager = (ViewPager)findViewById(R.id.activity_main_pager);
+        mPager.setAdapter(mAdapter);
+
+        mIndicator = (LinePageIndicator)findViewById(R.id.activity_main_pager_indicator);
+        mIndicator.setViewPager(mPager);
+
+    }
+
+    class MainActivityPagerAdapter extends FragmentPagerAdapter {
+
+        public MainActivityPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+
+            switch (position) {
+                case 0:
+                    return new DataTableAnytimeFragment();
+                case 1:
+                    return new StackedBarChartFragment();
+                case 2:
+                    return new StackedLineChartFragment();
+                default:
+                    throw new IllegalArgumentException("not this many fragments: " + position);
+            }
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return "";
+        }
+
+        @Override
+        public int getCount() {
+            return 3;
+        }
+
     }
 
 }
